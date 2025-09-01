e80ec504ab364b00789aed776d97a3ec0ea8caa8
Bob Witmer <bwitmer@golden.net>
2025-09-01 12:26:04 -0400

    Initial Commit

---
+++ FBIWanted/FBIWantedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBIWantedApp.swift
+//  FBIWanted
+//
+//  Created by Bob Witmer on 2025-09-01.
+//
+
+import SwiftUI
+
+@main
+struct FBIWantedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
